774c1ffe445d9bb756dd7b66ce3f9e17a4ccccd7
Elder Geovani Samayoa Esquivel <esamayoae1@miumg.edu.gt>
2026-03-01 20:14:50 -0600

    Add files via upload

---
+++ estructuraynodos.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// Nombre: Elder Geovani Samayoa Esquivel
+// Curso: Programación III
+// Hoja de trabajo 2
+
+struct Producto {
+    int codigo;
+    string nombre;
+    double precio;
+};
+
+struct Nodo {
+    Producto data;
+    Nodo* siguiente;
+};
+
+int main() {
+
+    Nodo* primero = new Nodo;
+
+    primero->data.codigo = 101;
+    primero->data.nombre = "Laptop";
+    primero->data.precio = 5500.50;
+
+    primero->siguiente = nullptr;
+
+    Nodo* segundo = new Nodo;
+
+    segundo->data.codigo = 102;
+    segundo->data.nombre = "Mouse";
+    segundo->data.precio = 150.75;
+
+    segundo->siguiente = nullptr;
+
+
+    primero->siguiente = segundo;
+
+
+    Nodo* tercero = new Nodo;
+
+    tercero->data.codigo = 103;
+    tercero->data.nombre = "Teclado";
+    tercero->data.precio = 300.00;
+
+    tercero->siguiente = nullptr;
+
+    segundo->siguiente = tercero;
+
+
+    Nodo* actual = primero;
+
+    while (actual != nullptr) {
+        cout << "Codigo: " << actual->data.codigo << endl;
+        cout << "Nombre: " << actual->data.nombre << endl;
+        cout << "Precio: Q" << actual->data.precio << endl;
+
+
+        actual = actual->siguiente;
+    }
+
+    actual = primero;
+
+    while (actual != nullptr) {
+        Nodo* temp = actual;
+        actual = actual->siguiente;
+        delete temp;
+    }
+
+    primero = nullptr;
+
+    return 0;
+}
